ece1341381d3ee3c988eacb04cc43a94fd0386ec
Urban Bruhin <urban.bruhin@sensirion.com>
2020-01-07 14:59:07 +0100

    Add class SensirionWordI2cCommand

--- sensirion_i2c_driver/__init__.py
+++ sensirion_i2c_driver/__init__.py
@@ -4,6 +4,7 @@
 from __future__ import absolute_import, division, print_function
 from .version import version as __version__  # noqa: F401
 from .command import I2cCommand  # noqa: F401
+from .sensirion_word_command import SensirionWordI2cCommand  # noqa: F401
 from .crc_calculator import CrcCalculator  # noqa: F401
 
 __copyright__ = '(c) Copyright 2019 Sensirion AG, Switzerland'
