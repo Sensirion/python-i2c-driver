909f9f7dbfd039de66f69f4188c536fde9d2432c
Rolf Laich <rolf.laich@sensirion.com>
2025-11-20 09:07:14 +0100

    Update project structure
    In order to be compatible with latest setuptools the project structure
    was updated.

--- sensirion_i2c_driver/version.py
+++ sensirion_i2c_driver/version.py
@@ -2,4 +2,8 @@
 # (c) Copyright 2019 Sensirion AG, Switzerland
 
 from __future__ import absolute_import, division, print_function
-version = "1.0.1"
+
+import importlib.metadata as metadata
+from typing import Final
+
+version: Final[str] = metadata.version("sensirion_i2c_driver")
